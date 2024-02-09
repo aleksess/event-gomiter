2e479d2d8679a7dae08573a1f2d1c33f8a151bb9
aleksess <and0941@gmail.com>
2024-02-10 00:17:54 +0100

    0.0.1: prealpha

---
+++ event-gomiter.go
@@ -0,0 +1,68 @@
+package eventgomiter
+
+type EventIdentifier string
+
+type Event interface{}
+
+type EventHandler func(Event)
+
+type EventHandleable interface {
+	EventHandler() (EventHandler, error)
+}
+
+type EventEmitter interface {
+	Emit(EventIdentifier, Event) error
+	RegisterHandler(EventIdentifier, EventHandler) error
+	RegisterHandleable(EventIdentifier, EventHandleable) error
+}
+
+type ChannelEventEmitter struct {
+	channels map[EventIdentifier]chan Event
+	handlers map[EventIdentifier][]EventHandler
+}
+
+var _ EventEmitter = (*ChannelEventEmitter)(nil)
+
+func NewChannelEventEmitter() *ChannelEventEmitter {
+	emitter := new(ChannelEventEmitter)
+
+	go func() {
+		for {
+			for eventId, channel := range emitter.channels {
+				event := <-channel
+				for _, handler := range emitter.handlers[eventId] {
+					go handler(event)
+				}
+			}
+		}
+	}()
+
+	return emitter
+}
+
+func (c *ChannelEventEmitter) Emit(id EventIdentifier, event Event) error {
+	c.channels[id] <- event
+	return nil
+}
+
+func (c *ChannelEventEmitter) RegisterHandler(id EventIdentifier, handler EventHandler) error {
+	if c.handlers[id] == nil {
+		c.handlers[id] = make([]EventHandler, 0, 1)
+	}
+	if c.channels[id] == nil {
+		c.channels[id] = make(chan Event)
+	}
+
+	c.handlers[id] = append(c.handlers[id], handler)
+
+	return nil
+}
+
+func (c *ChannelEventEmitter) RegisterHandleable(id EventIdentifier, handleable EventHandleable) error {
+	handler, err := handleable.EventHandler()
+	if err != nil {
+		return err
+	} else {
+		return c.RegisterHandler(id, handler)
+	}
+}
